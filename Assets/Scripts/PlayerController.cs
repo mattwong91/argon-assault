d71a5c1fa87467531509f04bb25820fc5ecc1bce
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-04-24 23:23:01 -0600

    Testing movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,5 +20,15 @@ public class PlayerController : MonoBehaviour
   {
     float xThrow = movement.ReadValue<Vector2>().x;
     float yThrow = movement.ReadValue<Vector2>().y;
+
+    float xOffset = 0.2f;
+
+    // transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y, transform.localPosition.z);
+
+    // NOTE My thoughts on how to do this movement, adding vector3 objects together
+    Vector3 currentPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+    Vector3 offsetPosition = new Vector3(xOffset, 0, 0);
+
+    transform.localPosition = currentPosition + offsetPosition;
   }
 }
