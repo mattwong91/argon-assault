98c0fceb4e6cc065836c774a98e769075cee5c18
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-04-30 03:50:16 -0600

    Debug logs for testing

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-
+  void OnCollisionEnter(Collision other)
+  {
+    Debug.Log(this.name + "--Collided with--" + other.gameObject.name);
+  }
+  void OnTriggerEnter(Collider other)
+  {
+    Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+  }
 }
