e729eef5907571c1ef8e03c84c7efd4aed805d17
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-01 02:20:37 -0600

    Enemy explosions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+  [SerializeField] GameObject deathVFX;
+  [SerializeField] Transform parent;
   void OnParticleCollision(GameObject other)
   {
-    Debug.Log($"{name} is hit by {other.gameObject.name}");
+    GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+    vfx.transform.parent = parent;
     Destroy(gameObject);
   }
 }
