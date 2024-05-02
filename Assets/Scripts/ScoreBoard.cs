6e06df682866dac10d7da6941d43ef628268b820
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-02 12:56:27 -0600

    Display score

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
   int score;
+  TMP_Text scoreText;
+
+  void Start()
+  {
+    scoreText = GetComponent<TMP_Text>();
+    scoreText.text = "Start";
+  }
 
   public void IncreaseScore(int amountToIncrease)
   {
     score += amountToIncrease;
-    Debug.Log($"Score: {score}");
+    scoreText.text = score.ToString();
   }
 
 }
