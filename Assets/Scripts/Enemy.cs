fcd57a21f9df2235abc6a07732263bb696eead06
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-02 15:51:01 -0600

    Implement enemy health and hit VFX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
   [SerializeField] GameObject deathVFX;
+  [SerializeField] GameObject hitVFX;
   [SerializeField] Transform parent;
 
   [SerializeField] int scoreValue;
+  [SerializeField] int hitPoints = 2;
 
   ScoreBoard scoreBoard;
 
@@ -17,21 +19,32 @@ public class Enemy : MonoBehaviour
   void OnParticleCollision(GameObject other)
   {
     ProcessHit();
-    KillEnemy();
+    if (hitPoints < 1)
+    {
+      KillEnemy();
+    }
   }
 
   void ProcessHit()
   {
     scoreBoard.IncreaseScore(scoreValue);
+    hitPoints--;
+    if (hitPoints >= 1)
+    {
+      PlayVFX(hitVFX);
+    }
   }
 
   void KillEnemy()
   {
-    GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-    vfx.transform.parent = parent;
-
+    PlayVFX(deathVFX);
     Destroy(gameObject);
   }
 
+  void PlayVFX(GameObject vfxToPlay)
+  {
+    GameObject vfx = Instantiate(vfxToPlay, transform.position, Quaternion.identity);
+    vfx.transform.parent = parent;
+  }
 
 }
